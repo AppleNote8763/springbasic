3965e7af34c504287527d708b0fcf9dc8505a78f
AppleNote <a7201492425@gmail.com>
2024-08-21 16:51:44 +0900

    pathVariable

--- src/main/java/com/cys/springbasic/controller/RequestDataContoller.java
+++ src/main/java/com/cys/springbasic/controller/RequestDataContoller.java
@@ -1,6 +1,7 @@
 package com.cys.springbasic.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,25 @@ public class RequestDataContoller {
     // http://localhost:4000/request-data/request-param?name=홍길동&age=30
     public String requestParam(
         // @RequestParam(name = "name") String name,
-        @RequestParam() String name,
+        @RequestParam("name") String name,
         @RequestParam(name = "age", required = false) Integer age
     ) {
-
         return "이름 : " + name + " 나이 : " + age;
+    }
 
+    // @PathVariable() :
+    // 모든 HTTP 메서드에서 URL의 특정 패턴에 따라 데이터를 추출
+    // GET http://localhost:4000/request-data/path-variable/*
+    @GetMapping({
+        "/path-variable/{var}",
+        "/path-variable/{var}/",
+        "/path-variable/{var}/{str}"
+    })
+    public String pathVariable(
+        @PathVariable(name = "var") String var,
+        @PathVariable(name = "str", required = false) String str
+    ) {
+        return "읽은 경로 변수 : " + var + ", " + str;
     }
 
 }
