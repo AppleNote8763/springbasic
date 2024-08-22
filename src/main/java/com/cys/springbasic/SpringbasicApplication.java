7854509aba3c2d6d1de0687ea58866e8888acadd
AppleNote <a7201492425@gmail.com>
2024-08-22 15:51:38 +0900

    2024.08.22. 15:51

--- src/main/java/com/cys/springbasic/SpringbasicApplication.java
+++ src/main/java/com/cys/springbasic/SpringbasicApplication.java
@@ -3,11 +3,28 @@ package com.cys.springbasic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cys.springbasic.dto.Lombok;
+
 @SpringBootApplication
 public class SpringbasicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbasicApplication.class, args);
+	}
+
+	void lombokExample() {
+
+		Lombok lombok = new Lombok("a", "b", "c", false, false);
+		lombok.getField1();
+		lombok.setField3(null);
+
+		// lombok = new Lombok();
+		lombok = new Lombok("d", "e");
+
+		// lombok.getField4();	// 기본형 boolean일 경우 getter 메서드 이름은 isXXX()
+		lombok.isField4();
+		lombok.getField5();
+
 	}
 
 }
