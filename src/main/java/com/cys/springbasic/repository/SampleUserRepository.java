b15e7beea0eabfc1b20763171f844071309b8542
AppleNote <a7201492425@gmail.com>
2024-08-30 17:21:16 +0900

    2024.08.30. 17:21

--- src/main/java/com/cys/springbasic/repository/SampleUserRepository.java
+++ src/main/java/com/cys/springbasic/repository/SampleUserRepository.java
@@ -64,4 +64,6 @@ extends JpaRepository<SampleUserEntity, String> {
         @Param("address") String address
     );  // "SELECT * " + "FROM sample_user " + "WHERE name = :name " + "AND address = :address" 공백을 포함시켜줘야함
 
+    SampleUserEntity findByUserId(String userId);
+
 }
--- src/main/java/com/cys/springbasic/service/SampleService.java
+++ src/main/java/com/cys/springbasic/service/SampleService.java
@@ -10,5 +10,6 @@ public interface SampleService {
     ResponseEntity<String> deleteSample1 (String sampleId);
     ResponseEntity<String> queryMethod();
     String getJwt(String name);
+    String validateJwt(String jwt);
 
 }
