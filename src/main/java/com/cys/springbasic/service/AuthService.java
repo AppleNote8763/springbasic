5dbaa0fa3aa1a539ba0986bc7ed6a2b65719dd7d
AppleNote <a7201492425@gmail.com>
2024-09-04 17:44:13 +0900

    2024.09.04. 17:44

--- src/main/java/com/cys/springbasic/service/AuthService.java
+++ src/main/java/com/cys/springbasic/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.cys.springbasic.service;
 
 import com.cys.springbasic.dto.PostUserRequestDto;
+import com.cys.springbasic.dto.SignInRequestDto;
 
 public interface AuthService {
     String signUp(PostUserRequestDto dto);
+    String signIn(SignInRequestDto dto);
 }
